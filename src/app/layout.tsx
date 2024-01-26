6c6de5919395420c693cbfdceaf69c08f400b854
Jason Telanoff <json22@berkeley.edu>
2024-01-26 13:08:44 -0800

    Update metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,22 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Telanoff",
-  description: "Software Engineer",
+  title: "Jason Telanoff",
+  description: "A web portfolio for me, Jason Telanoff",
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/icon_light.png',
+        href: '/icon_light.png',
+      },
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: '/icon_dark.png',
+        href: '/icon_dark.png',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
