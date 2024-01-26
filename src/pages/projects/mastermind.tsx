2b03a75cc7543139546b05ee0510719a33ba100e
Jason Telanoff <json22@berkeley.edu>
2024-01-25 22:44:44 -0800

    Update project descriptions and links

--- src/pages/projects/mastermind.tsx
+++ src/pages/projects/mastermind.tsx
@@ -7,11 +7,12 @@ export default function Mastermind() {
         <div className="mx-32 p-16 flex flex-col items-center justify-center align-middle">
             <Navbar />
             <h1 className="text-4xl my-8">Mastermind</h1>
-            <p className="text-lg mb-8">A simple Mastermind game with a history. I coded this over half a decade ago over a weekend in Khan Academy's processing js.
-                It's really cool to see what I used to believe were cutting edge tricks and techniques. In order to have a playable demo here, I had to convert it to p5.js and then bring that into react.
-                It's stuck to the left because of the package I'm using to do this. I might make do this without the package in the future, so stay tuned!
-                I didn't make the code more efficient, so what you're playing with here is practically what is shown on the github.
-                Maybe I'll make a modern version in the future. Enjoy!</p>
+            <p className="text-lg mb-8">A simple Mastermind game with a history. I coded this over half a decade ago over a weekend in Khan Academy&apos;s processing.js. It&apos;s fascinating to revisit what I once considered cutting-edge tricks and techniques. To provide a playable demo here, I had to convert it to p5.js and then integrate it into React.
+
+                The game interface is currently aligned to the left due to the package I&apos;m using for integration. I may explore alternatives in the future, so stay tuned for updates!
+
+                I also didn&apos;t optimize the code for efficiency, so what you experience here is essentially what&apos; on the GitHub repository. Perhaps in the future, I&apos;ll create a modernized version. Enjoy the game!
+            </p>
 
             <MastermindGame />
         </div>
