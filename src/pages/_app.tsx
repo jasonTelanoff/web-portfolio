add36d5c8e1a055ebeb8c53152263e0c9f7aa6df
Jason Telanoff <json22@berkeley.edu>
2024-01-31 19:13:35 -0800

    trying to fix the website

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../app/globals.css';
-import type { Metadata } from 'next'
-
-export const metadata: Metadata = {
-    title: 'Hello World!',
-    description: '...',
-}
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
