8f05999db82f7da7cc0bab2d694d0e8fa4f4b823
Jason Telanoff <57235796+jasonTelanoff@users.noreply.github.com>
2024-07-02 12:42:07 -0700

    Update about_me.tsx

--- src/pages/about_me.tsx
+++ src/pages/about_me.tsx
@@ -82,7 +82,7 @@ export default function AboutMe() {
                     />
                     <div>
                         <p className="text-lg mb-6">
-                            Hello! I&apos;m Jason Telanoff, a passionate software engineer and a Computer Science and Applied Math double major at UC Berkeley expected to graduate in Fall 2024. I have a strong background in mobile development and efficient algorithms, but enjoy working on a variety of projects.
+                            Hello! I&apos;m Jason Telanoff, a passionate software engineer and a Computer Science and Applied Math double major at UC Berkeley expected to graduate a year and a half early in Fall 2024. I have a strong background in mobile development and efficient algorithms, but enjoy working on a variety of projects.
                         </p>
                         <p className="text-lg mb-6">
                             My journey in software development started in elementary school, when my father showed me a basic Scratch game. Since then, I&apos;ve been eager to learn new technologies and explore innovative solutions to challenging problems.
